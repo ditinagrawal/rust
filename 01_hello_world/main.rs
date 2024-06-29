405e196568ef556d91c0d868264e62415624d70d
ditinagrawal <ditin.agrawal05@gmail.com>
2024-06-29 05:53:45 +0530

    Hello World

---
+++ 01_hello_world/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+//? N O T E S
+//* `fn` is a keyword which is used to declare a function */
+//* `main` is the name of the function which is the entry point of the program */
+//* `println!` is a macro which prints the given text to the standard output */
