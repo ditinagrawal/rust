1bce2b6815a26e42dba65ce449d3485831e4d16f
ditinagrawal <ditin.agrawal05@gmail.com>
2024-06-29 06:03:59 +0530

    Cargo

---
+++ 02_cargo/main/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+//? N O T E S
+
+//* `cargo` is a package manager for Rust */
+//* To create a new project using `cargo` we can use `cargo new dirname` */
+//* We can compile our code by using `cargo build` */
+//* We can run our code by using `cargo run` */
+//* We can check our code for errors by using `cargo check` */
+//* `cargo` creates a `Cargo.toml` file which is just like `package.json` in `node.js` */
+//TODO: How to add dependencies in `Cargo.toml` file?
+//* We can add dependencies in `Cargo.toml` file by adding then below `[dependencies]` section */
+//* Like `rand = "0.8.5"` for random generation of numbers */
+//* We can install our dependencies by using `cargo build` */
+//* We can update our dependencies by using `cargo update` */
