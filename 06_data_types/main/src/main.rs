299614a14e96694e37c4ea4e38b950132fb7fa81
ditinagrawal <ditin.agrawal05@gmail.com>
2024-07-04 11:34:42 +0530

    Data Types

---
+++ 06_data_types/main/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    // Integer types
+    let a: i8 = 10;
+    let b: i16 = 200;
+    let c: i32 = 300000;
+    let d: i64 = 4000000000;
+    let e: i128 = 500000000000000;
+
+    // Floating point types
+    let f: f32 = 3.14;
+    let g: f64 = 3.1415926535897932384626433832;
+
+    // Boolean type
+    let h: bool = true;
+    let i: bool = false;
+
+    // Character type
+    let j: char = 'a';
+    let k: char = '🚀';
+
+    // String type
+    let l: &str = "Hello, World!";
+    let m: String = String::from("Hello, World!");
+}
+
+//? N O T E S
+/*
+Length  	Signed  	Unsigned
+8-bit   	i8  	    u8
+16-bit  	i16 	    u16
+32-bit  	i32 	    u32
+64-bit  	i64 	    u64
+128-bit 	i128    	u128
+*/
+//* f32 , f64 for floating point numbers */
---
+++ 06_data_types/main/target/debug/main.d
@@ -0,0 +1,1 @@
+/Users/ditinagrawal/Rust/06_data_types/main/target/debug/main: /Users/ditinagrawal/Rust/06_data_types/main/src/main.rs
